7203067151408e3b47365952f9e1c22ef2df227a
eesch001 <eesch001@ucr.edu>
2020-07-20 14:56:57 -0700

    Updates

---
+++ precache-manifest.43a21e5e9ae39a02b3e5af6c49ab46a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1743f88060230b9146f5abc45876c54",
+    "url": "/QuizQuestions/index.html"
+  },
+  {
+    "revision": "1d75b190e3786dd40d98",
+    "url": "/QuizQuestions/static/css/main.ffe8b069.chunk.css"
+  },
+  {
+    "revision": "f770e2f2e9678ca0da0f",
+    "url": "/QuizQuestions/static/js/2.fa28002b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QuizQuestions/static/js/2.fa28002b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d75b190e3786dd40d98",
+    "url": "/QuizQuestions/static/js/main.25fe9965.chunk.js"
+  },
+  {
+    "revision": "65d6557c070ea1393ee7",
+    "url": "/QuizQuestions/static/js/runtime-main.9f9b2d71.js"
+  }
+]);
